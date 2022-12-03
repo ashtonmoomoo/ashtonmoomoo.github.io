c706c65aeef0b99206e5f3c3017ff5b9f0e9c053
Ashton Moore <ashton.chris.moore@gmail.com>
2022-12-03 20:40:53 +1300

    Sort posts by date desc

--- web/content/index.ts
+++ web/content/index.ts
@@ -1,7 +1,9 @@
 import { etaConversion } from "./posts/etaConversion";
 import type { Post } from "./types";
 
-const content: Post[] = [etaConversion];
+const content: Post[] = [etaConversion].sort(
+  (a, b) => Number(b.posted) - Number(a.posted)
+);
 
 export { Post };
 export default content;
