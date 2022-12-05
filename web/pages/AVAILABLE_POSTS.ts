abf2d7f203efb50567a0ccf3029ca3f484bc3f55
Ashton Moore <ashton.chris.moore@gmail.com>
2022-12-05 20:40:07 +1300

    Read posts from `main`

--- web/pages/AVAILABLE_POSTS.ts
+++ web/pages/AVAILABLE_POSTS.ts
@@ -7,7 +7,7 @@ export interface PostMeta {
 }
 
 // Need to sort out a better way to do this
-const BRANCH = "posts-round-3";
+const BRANCH = "main";
 const BASE_URL = `https://raw.githubusercontent.com/ashtonmoomoo/ashtonmoomoo.github.io/${BRANCH}/_posts/`;
 export const AVAILABLE_POSTS: PostMeta[] = [
   {
